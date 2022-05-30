747cd9e1d7844a167df3e48d3c4d3c9cec74bf9d
ryota-09 <n.ryo950911@gmail.com>
2022-05-30 16:01:22 +0900

    start

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Home from '../pages/index'
 
-it('Should render hello text', () => {
+it('Test', () => {
   render(<Home />)
   expect(screen.getByText('Hello Nextjs')).toBeInTheDocument()
 })
