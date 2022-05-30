267518c93f28a20c08decc54cdad416439be583b
ryota-09 <n.ryo950911@gmail.com>
2022-05-30 16:35:41 +0900

    ナビゲーションバーの正常系のテストコード実装

---
+++ __tests__/Layout.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { getPage } from 'next-page-tester'
+import { initTestHelpers } from 'next-page-tester'
+import { findByText } from '@storybook/testing-library'
+
+initTestHelpers()
+
+describe('components/Layout.tsx', () => {
+  test('正常系: ナビゲーションバーをクリックすると各ページに正しく遷移する。', async () => {
+    const { page } = await getPage({
+      route: '/',
+    });
+    render(page);
+
+    await userEvent.click(screen.getByTestId("about-nav"));
+    expect(await screen.findByText("About")).toBeInTheDocument();
+    await userEvent.click(screen.getByTestId("contact-nav"));
+    expect(await screen.findByText("Contact")).toBeInTheDocument();
+    await userEvent.click(screen.getByTestId("signin-nav"));
+    expect(await screen.findByText("Sign In")).toBeInTheDocument();
+  });
+})
