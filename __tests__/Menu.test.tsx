3a69f9f3ce494d731a7ede8c9873d8ca7d60768d
ryota-09 <n.ryo950911@gmail.com>
2022-06-01 15:54:14 +0900

    Menuコンポーネントの正常系のユニットテスト作成

---
+++ __tests__/Menu.test.tsx
@@ -0,0 +1,24 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { ReactNode } from "react";
+import Menu from "../components/molecules/Menu";
+
+type Props = {
+  menuTitle: string;
+}
+
+describe("molcules/Menu.tsx", () => {
+  test("正常系: コンポーネントが正しくレンダリングされる", () => {
+    let dummyProps: Props;
+    dummyProps = {
+      menuTitle: "dummy Title"
+    }
+    render(
+      <Menu>
+        <span>dummyComp</span>
+      </Menu>
+    );
+    expect(screen.findByText("dummy Title")).toBeInTheDocument();
+    expect(screen.findByText("dummyComp")).toBeInTheDocument();
+  });
+});
