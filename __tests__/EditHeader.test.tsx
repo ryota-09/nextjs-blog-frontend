f989b692e87cff8181b6e891486b3af4dd477ca8
ryota-09 <n.ryo950911@gmail.com>
2022-06-02 11:48:45 +0900

    EditHeaderコンポーネントのユニットテストpass

--- __tests__/EditHeader.test.tsx
+++ __tests__/EditHeader.test.tsx
@@ -1,10 +1,23 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import { Dispatch, SetStateAction } from "react";
 import EditHeader from "../components/organisms/EditHeader";
 
+type Props = {
+  setHeaderTitle: Dispatch<SetStateAction<string>>;
+  setHeaderImg: Dispatch<SetStateAction<string>>;
+  setHeaderSummary: Dispatch<SetStateAction<string>>;
+};
+
 describe("templates/EditHeader.tsx", () => {
-  test("正常系: コンポーネントが正しくレンダリングされる", () => {
-    render(<EditHeader />);
-    expect(screen.findByText("Edit Area")).toBeInTheDocument();
+  let dummyProps: Props;
+  dummyProps = {
+    setHeaderTitle: jest.fn(),
+    setHeaderImg: jest.fn(),
+    setHeaderSummary: jest.fn()
+  }
+  test("正常系: コンポーネントが正しくレンダリングされる", async () => {
+    render(<EditHeader {...dummyProps} />);
+    expect(await screen.findByText("Article Title")).toBeInTheDocument();
   });
 });
