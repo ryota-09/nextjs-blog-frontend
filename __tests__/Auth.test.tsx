fcb1080c15a0f23998007aa7119136653cc85a3e
ryota-09 <n.ryo950911@gmail.com>
2022-06-03 10:09:38 +0900

    Authコンポーネントのユニットテストの記述

--- __tests__/Auth.test.tsx
+++ __tests__/Auth.test.tsx
@@ -41,8 +41,8 @@ afterAll(() => {
   server.close()
 })
 
-describe('adminページのテスト', () => {
-  it('ログインしている状態の時、indexページに遷移できる', async () => {
+describe('organisms/Auth.tsx', () => {
+  it('正常系: ログインしている状態の時、indexページに遷移できる', async () => {
     const { page } = await getPage({
       route: '/admin-page',
     })
@@ -51,75 +51,27 @@ describe('adminページのテスト', () => {
     userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
     userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
 
-    userEvent.click(screen.getByText('Login with JWT'))
     expect(await screen.findByText('blog page')).toBeInTheDocument()
   })
-  it('ログイン失敗した時、そのページから遷移させない', async () => {
+  it('異常系: ログイン失敗した時、そのページから遷移させない', async () => {
     server.use(
       rest.post(
-        `${process.env.NEXT_PUBLIC_RESTAPI_URL}/jwt/create/`,
+        "ここにurl",
         (req, res, ctx) => {
           return res(ctx.status(400))
         }
       )
     )
     const { page } = await getPage({
-      route: '/admin-page',
+      route: '/signin',
     })
     render(page)
     expect(await screen.findByText('Login')).toBeInTheDocument()
     userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
     userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
-    userEvent.click(screen.getByText('Login with JWT'))
+    
     expect(await screen.findByText('Login Error')).toBeInTheDocument()
     expect(screen.getByText('Login')).toBeInTheDocument()
-    expect(screen.queryByText('blog page')).toBeNull()
-  })
-  it('サインアップ画面に切り替わるかどうか', async () => {
-    const { page } = await getPage({
-      route: '/admin-page',
-    })
-    render(page)
-
-    expect(await screen.findByText('Login')).toBeInTheDocument()
-    expect(screen.getByText('Login with JWT')).toBeInTheDocument()
-    userEvent.click(screen.getByTestId('mode-change'))
-    expect(screen.getByText('Sign up')).toBeInTheDocument()
-    expect(screen.getByText('Create new user')).toBeInTheDocument()
-  })
-  it('ユーザー登録が成功した時、ログインも同時に行える', async () => {
-    const { page } = await getPage({
-      route: '/admin-page',
-    })
-    render(page)
-
-    expect(await screen.findByText('Login')).toBeInTheDocument()
-    userEvent.click(screen.getByTestId('mode-change'))
-    userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
-    userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
-    userEvent.click(screen.getByText('Create new user'))
-    expect(await screen.findByText('blog page')).toBeInTheDocument()
-  })
-  it('ユーザー登録が失敗したときのテスト', async () => {
-    server.use(
-      rest.post(
-        `${process.env.NEXT_PUBLIC_RESTAPI_URL}/register/`,
-        (req, res, ctx) => {
-          return res(ctx.status(400))
-        }
-      )
-    )
-    const { page } = await getPage({
-      route: '/admin-page',
-    })
-    render(page)
-    expect(await screen.findByText('Login')).toBeInTheDocument()
-    userEvent.click(screen.getByTestId('mode-change'))
-    userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
-    userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
-    userEvent.click(screen.getByText('Create new user'))
-    expect(await screen.findByText('Registration Error'))
-    expect(screen.getByText('Sign up')).toBeInTheDocument()
-    expect(screen.queryByText('blog page')).toBeNull()
+    expect(screen.queryByText('Nextjs Blog')).toBeNull()
   })
 })
